b24b84c394412de5fae570cb5d1d5643588d5ef6
siraj <sirajmuzeyin1@gmail.com>
2024-02-17 22:29:38 +0300

    My first commit

---
+++ firstGitCode.java
@@ -0,0 +1,18 @@
+class Hello
+{
+
+    public static void main(String a[])
+    {
+        int x=8;
+        int y = 5;
+        int z = 3;
+        if(x>y && x>z)
+        {
+            System.out.println(x);
+            System.out.println("Good bye");
+        }
+        else
+        System.out.println(y);
+    }
+
+}   
